86290140b9cf43007cba3eb773ee92e34ad4de67
Bryan Bugyi <bryanbugyi34@gmail.com>
2021-09-23 13:07:31 -0400

    Add docstrings

--- tests/test_xdg.py
+++ tests/test_xdg.py
@@ -1,3 +1,5 @@
+"""Tests for the bugyi.lib.xdg module."""
+
 import os
 from pathlib import Path
 
@@ -17,6 +19,7 @@ xdg_params = [(x, Path(y)) for x, y in _xdg_params]
 
 @pytest.mark.parametrize("key, expected_part", xdg_params)
 def test_xdg_init(key: xdg.XDG_Type, expected_part: Path) -> None:
+    """Test the xdg.init_full_dir() function."""
     expected = expected_part / "test_xdg"
     assert expected == xdg.init_full_dir(key)
     os.rmdir(expected)
@@ -24,9 +27,11 @@ def test_xdg_init(key: xdg.XDG_Type, expected_part: Path) -> None:
 
 @pytest.mark.parametrize("key, expected", xdg_params)
 def test_xdg_get_base_dir(key: xdg.XDG_Type, expected: Path) -> None:
+    """Test the xdg.get_base_dir() function."""
     assert expected == xdg.get_base_dir(key)
 
 
 def test_init_failure() -> None:
+    """Test that the xdg.init_full_dir() fails when given a bad argument."""
     with pytest.raises(AssertionError):
         xdg.init_full_dir("bad_key")  # type: ignore
