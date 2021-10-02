8a338fe984c1bbf32bf0b74320d4f780a30aa9f5
Bryan Bugyi <bryanbugyi34@gmail.com>
2021-10-02 12:50:51 -0400

    Update cc via 'cruft update'

--- src/bugyi/lib/__init__.py
+++ src/bugyi/lib/__init__.py
@@ -1,5 +1,10 @@
 """Overly general Python library used when no other library is a good fit."""
 
+import logging as _logging
+
+
 __author__ = "Bryan M Bugyi"
 __email__ = "bryanbugyi34@gmail.com"
 __version__ = "0.6.0"
+
+_logging.getLogger(__name__).addHandler(_logging.NullHandler())
