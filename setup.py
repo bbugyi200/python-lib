932bc1a5559144c14e7d8d2b1eb296865d58213c
Bryan Bugyi <bryanbugyi34@gmail.com>
2021-12-20 14:36:16 -0500

    Remove extra set of Python classifiers

--- setup.py
+++ setup.py
@@ -87,9 +87,6 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
     ]
     + [
         f"Programming Language :: Python :: {pretty_pyver}"
